81ae721b7a73791e8dd9ce335d3a719ab374287b
egpfls <wmfdlfpdy@gmail.com>
2021-03-30 16:01:13 +0900

    ..완성1

---
+++ js/index.js
@@ -0,0 +1,42 @@
+$(window).scroll(function(){
+	var scTop = $(this).scrollTop()
+	if(scTop > 200) {
+		// if(!$('.header-wrapper').hasClass('active')) {
+		// 	$('.header-wrapper').css('top', '-60px')
+		// 	$('.header-wrapper').stop().animate({'top': 0}, 300)
+		// 	$('.header-wrapper').addClass('active')
+		// }
+
+		$('.header-wrapper').css('transform', 'translateY(-100%)')
+		$('.header-wrapper').css('transform')
+		$('.header-wrapper').css('transform', 'translateY(0)')
+		$('.header-wrapper').addClass('active')
+
+		// $('.header-wrapper').css('top', '-60px')
+		// $('.header-wrapper').stop().animate({'top' : 0}, 300)
+		// $('.header-wrapper').addClass('active')
+	}
+	else {
+		$('.header-wrapper').css('transform', 'translateY(0)')
+		$('.header-wrapper').removeClass('active')
+	}
+})
+
+$('.mo-bars').click(function() {
+	$('.monavi-wrapper').toggleClass('active')
+})
+
+$('.modal-wrapper').click(function(){
+	$(this).css('display', 'none')
+})
+
+$('.pf-wrap > .pf > img').click(function(){
+	var src = $(this).attr('src')
+	var desc = $(this).attr('alt')
+	$('.modal-wrapper').css('display', 'block')
+	$('.modal-wrapper .modal-wrap').css('transform', 'scale(0)')
+	$('.modal-wrapper .modal-wrap').css('transform')
+	$('.modal-wrapper .modal-img').attr('src', src)
+	$('.modal-wrapper .modal-title').text(desc)
+	$('.modal-wrapper .modal-wrap').css('transform', 'scale(1)')
+})
